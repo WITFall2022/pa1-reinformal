6c8147d7c4b754c82f277e660b1ef16c7aadf117
324250 <324250@664-161473-LAB.schools.mybps.org>
2022-10-31 11:57:14 -0400

    :V

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,4 +1,5 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
 // TODO: document this class
 public class PA1a {
@@ -6,6 +7,31 @@ public class PA1a {
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+	
+
+				// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		int yards;
+		int feet;
+		int inches;
+
+		System.out.printf("Enter the amount of yards: ");
+		yards = input.nextInt();
+
+		System.out.printf("Enter the amount of feet: ");
+		feet = input.nextInt();
+
+		System.out.printf("Enter the amount of inches:");
+		inches = input.nextInt();
+
+
+
+
+		int ans; 
+		ans=feet*12+yards*36+inches;
+		System.out.print("Total number of inches:"+ans);
+
 	}
 
 }
